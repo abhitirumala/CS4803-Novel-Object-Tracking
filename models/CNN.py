7270ce941aa94a74a9566bcba928c5bef5d22057
Tirumala, Abhiram Venkata <abhiram.tirumala@gatech.edu>
2021-12-07 14:30:20 -0500

    Increased model size

--- models/CNN.py
+++ models/CNN.py
@@ -5,32 +5,33 @@ import torch.nn.functional as F
 
 class CNNForecaster(nn.Module):
     
-    def __init__(self, input_shape, output_shape, num_layers, dropout):
+    def __init__(self, input_shape, output_shape, num_layers, dropout, embedding_size=32):
         super(CNNForecaster, self).__init__()
         self.input_shape = input_shape
         self.output_shape = output_shape
         self.num_layers = num_layers
         self.dropout = dropout
+        self.embedding_size = embedding_size
         
-        self.embedding = nn.Linear(input_shape[0] * input_shape[1], input_shape[1] * 32)
+        self.embedding = nn.Linear(input_shape[0] * input_shape[1], input_shape[1] * self.embedding_size)
         
-        self.conv1 = nn.Conv1d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv1d(self.embedding_size, self.embedding_size, kernel_size=3, stride=1, padding=1)
         
         self.conv_layers = []
         
         for i in range(1, num_layers):
-            self.conv_layers.append(nn.Conv1d(32, 32, kernel_size=3, stride=1, padding=1))
+            self.conv_layers.append(nn.Conv1d(self.embedding_size, self.embedding_size, kernel_size=3, stride=1, padding=1))
         
         self.max_pool = nn.MaxPool1d(kernel_size=3, stride=1, padding=1)
         
-        self.fc = nn.Linear(32 * input_shape[1], output_shape[0] * output_shape[1])
+        self.fc = nn.Linear(self.embedding_size * input_shape[1], output_shape[0] * output_shape[1])
         self.dropout = nn.Dropout(dropout)
     
     def forward(self, x):
         
         x = x.flatten(start_dim=1)
         embedded_x = self.embedding(x)
-        embedded_x = embedded_x.view(embedded_x.size(0), self.input_shape[1], 32)
+        embedded_x = embedded_x.view(embedded_x.size(0), self.input_shape[1], self.embedding_size)
         embedded_x = embedded_x.permute(0, 2, 1)
         
         output = self.max_pool(F.relu(self.conv1(embedded_x)))
@@ -59,7 +60,7 @@ if __name__ == "__main__":
     output shape: (2, 8)
     num_layers: 4
     """
-    model = CNNForecaster((2, 8), (2, 8), 4, 0.5)
+    model = CNNForecaster((2, 8), (2, 8), 4, 0.5, 32)
     test_tensor = torch.randn(32, 2, 8)
     
     output = model(test_tensor)
